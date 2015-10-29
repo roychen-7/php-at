163d5d545bc513c94affeafe34f874b57e6adff0
roychen <roychad.cy7@gmail.com>
2015-10-29 21:13:18 +0800

    .

--- test.php
+++ test.php
@@ -1,23 +1,10 @@
 <?php
 
 try {
-    $file = @fopen('a.txt', 'r');
-    if ($file) {
-        fread($file, '10');
-        fclose($file);
-    } else {
-        throw new Exception('Can not open file');
-    }
-} catch(Exception $e) {
-    var_dump($e);
+	mkdir('/Users/roychen/Dev/php-at/logs', 0777);
+} catch (Exception $e) {
+	var_dump($e);
 }
 
-try {
-    $file = fopen('a.txt', 'r');
-        fread($file, '10');
-        fclose($file);
-} catch(Exception $e) {
-    var_dump($e);
-}
 
 ?>
